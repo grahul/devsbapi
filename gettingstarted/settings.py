ff27b2905004ee4be8991b99fd89352203ce6501
grahul <rahulg.active@gmail.com>
2016-02-15 10:25:50 +0530

    BASIC_WWW_AUTHENTICATION REMOVE

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -18,7 +18,7 @@ import dj_database_url
 # Application definition
 BASIC_WWW_AUTHENTICATION_USERNAME = "papa10"
 BASIC_WWW_AUTHENTICATION_PASSWORD = "papa10"
-BASIC_WWW_AUTHENTICATION = True
+# BASIC_WWW_AUTHENTICATION = True
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
